d58b08a93991b67bfe5797c89925451698e4a0e6
Santi-004 <agustinsalassanti@gmail.com>
2025-06-14 00:01:38 -0300

    Se agregaron limitaciones a los campos del formulario y alertas. También se centro el formulario.

--- src/components/TurnoFormulario.jsx
+++ src/components/TurnoFormulario.jsx
@@ -13,6 +13,19 @@ const FechaActual = () => {
     return `${yyyy}-${mm}-${dd}T${hh}:${min}`;
 };
 
+const FechaMax = () => {
+    const fechamax = new Date(); //Nueva fecha
+    fechamax.setDate(fechamax.getDate() + 7);
+    const yyyy = fechamax.getFullYear(); //Año de la fecha
+    const mm = String(fechamax.getMonth() + 1).padStart(2, '0'); //Mes de la fecha, se le agrega un 0 al inicio si la cadena de caracter es menor a 2
+    const dd = String(fechamax.getDate()).padStart(2, '0'); //Día de la fecha, se le agrega un 0 al inicio si la cadena de caracter es menor a 2
+    const hh = String(fechamax.getHours()).padStart(2, '0'); //Hora de la fecha, se le agrega un 0 al inicio si la cadena de caracter es menor a 2
+    const min = String(fechamax.getMinutes()).padStart(2, '0'); //Minutos de la fecha, se le agrega un 0 al inicio si la cadena de caracter es menor a 2
+    return `${yyyy}-${mm}-${dd}T${hh}:${min}`;
+};
+
+
+
 //Estados de los campos del formulario.
 const TurnoFormulario = () =>{
     const[nombre, setNombre] = useState("");
@@ -28,31 +41,39 @@ const handleSubmit = (e) =>{
 
 
     if(nombre.trim() === ""){
-        alert("Campo nombre no completado")
+        alert("Campo nombre no completado");
         return;
     }
     
     
     if(dni.trim() === ""){
-        alert("Campo DNI no completado")
+        alert("Campo DNI no completado");
         return;
+    }
+
+    const DNI =  Number(dni);
+
+    if ( DNI < 0) {
+      alert("El DNI no puede ser negativo.");
+      return;
     }
 
     
     if(fecha.trim() === ""){
-        alert("Campo fecha no completado")
+        alert("Campo fecha no completado");
         return;
+    }
+
+    const fechausuario = new Date(fecha);
+    const fechamax = new Date(FechaMax());
+    
+    if(fechausuario > fechamax){
+        alert(`La fecha no es valida, fecha máxima: ${fechamax}`);
     }
 
     
     if(motivo.trim() === ""){
-        alert("Campo motivo no completado")
-        return;
-    }
-
-    
-    if(aclaracion.trim() === ""){
-        alert("Campo aclaración no completado")
+        alert("Campo motivo no completado");
         return;
     }
 
@@ -75,47 +96,52 @@ const restaurar = () => {
 
 //Estructura del formulario 
 return(
-    <form onSubmit={handleSubmit} className="formu">
-        
-        <div className="formtxt">Formulario de turno</div>
+    <div className="formulario">
+        <form onSubmit={handleSubmit} className="formu">
+            
+            <div className="formtxt">Formulario de turno</div>
 
+                <div>
+                <label>Nombre</label>
+                <input type="text"
+                value={nombre}
+                onChange={(e)=>setNombre(e.target.value)}/>
+            </div>
             <div>
-            <label>Nombre</label>
-            <input type="text"
-            value={nombre}
-            onChange={(e)=>setNombre(e.target.value)}/>
-        </div>
-        <div>
-            <label>DNI:</label>
-            <input type="number"
-            value={dni}
-            onChange={(e)=>setDNI(e.target.value)}/>
-        </div>
-        <div>
-            <label>Fecha:</label>
-            <input type="datetime-local"
-            value={fecha}
-            onChange={(e)=>setFecha(e.target.value)}/>
-        </div>
-        <div>
-            <label>Motivo:</label>
-            <select value={motivo} onChange={(e)=>setMotivo(e.target.value)}>
-                <option value="">ELEGIR MOTIVO</option>
-                <option value="opcion1">Hablar con un preceptor.</option>
-                <option value="opcion2">Hablar con un regente.</option>
-                <option value="opcion3">Hablar con un directivo.</option>
-            </select>
-        </div>
-        <div>
-            <label>Aclaración:</label>
-            <input type="text"
-            value={aclaracion}
-            onChange={(e)=>setAclaracion(e.target.value)}/>
-        </div>
+                <label>DNI:</label>
+                <input type="text" maxLength={8}
+                value={dni}
+                onChange={(e)=>setDNI(e.target.value)}/>
+            </div>
+            <div>
+                <label>Fecha:</label>
+                <input type="datetime-local"
+                value={fecha}
+                onChange={(e)=>setFecha(e.target.value)}
+                min={FechaActual()}
+                />
+                
+            </div>
+            <div>
+                <label>Motivo:</label>
+                <select value={motivo} onChange={(e)=>setMotivo(e.target.value)}>
+                    <option value="">----------Elegir Motivo----------</option>
+                    <option value="opcion1">Hablar con un preceptor.</option>
+                    <option value="opcion2">Hablar con un regente.</option>
+                    <option value="opcion3">Hablar con un directivo.</option>
+                </select>
+            </div>
+            <div>
+                <label>Aclaración:</label>
+                <textarea rows="4" cols="5" maxLength={300}
+                value={aclaracion}
+                onChange={(e)=>setAclaracion(e.target.value)}/>
+            </div>
 
-        <button className="rest" type="button" onClick={restaurar}>Restaurar</button>
-        <button className="subt" type="submit">Enviar</button>
-    </form>
+            <button className="rest" type="button" onClick={restaurar}>Restaurar</button>
+            <button className="subt" type="submit">Enviar</button>
+        </form>
+    </div>
 );
 };
 
