34c5821dec8541761c822166ffdc61f5bee247bf
Santi-004 <agustinsalassanti@gmail.com>
2025-09-13 03:05:19 -0300

    Modificación del formulario turno y addDoc.

--- src/components/TurnoFormulario.jsx
+++ src/components/TurnoFormulario.jsx
@@ -1,5 +1,7 @@
-import React, {useState} from "react";
+import React, {useState,} from "react";
 import { useNavigate  } from "react-router-dom";
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../config/firebase';
 import "../css/Formulario.css";
 
 //Función para establecer la fecha actual como fecha predeterminada.
@@ -13,6 +15,7 @@ const FechaActual = () => {
     return `${yyyy}-${mm}-${dd}T${hh}:${min}`;
 };
 
+// Función para fecha máxima (7 días después)
 const FechaMax = () => {
     const fechamax = new Date(); //Nueva fecha
     fechamax.setDate(fechamax.getDate() + 7);
@@ -27,73 +30,81 @@ const FechaMax = () => {
 
 
   //Estados de los campos del formulario.
-    const TurnoFormulario = () =>{
-    const[nombre, setNombre] = useState("");
-    const[dni, setDNI] = useState("");
-    const[fecha, setFecha] = useState(FechaActual());
-    const[motivo, setMotivo] = useState("");
+const TurnoFormulario = () =>{
+  const[nombre, setNombre] = useState("");
+  const[apellido, setApellido] = useState("");
+  const[dni, setDNI] = useState("");
+  const[fecha, setFecha] = useState(FechaActual());
+  const[motivo, setMotivo] = useState("");
 
-    const navigate = useNavigate(); //Eli:variable para guardar el uso del navigate
+const navigate = useNavigate(); //Eli:variable para guardar el uso del navigate
 
   //Alertas para errores del usuario
     const [errores, setErrores] = useState({});
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
     e.preventDefault();
     const errores = {};
 
-  //Alerta nombre
-  if (nombre.trim() === "") {
-    errores.nombre = "Campo nombre no completado";
-  }
+    //Alerta nombre
+    if (nombre.trim() === "") {errores.nombre = "Campo nombre no completado";}
 
-  //Alerta dni
-  if (dni.trim() === "") {
-    errores.dni = "Campo DNI no completado";
-  } else {
-    const DNI = Number(dni);
-    if (isNaN(DNI)) {
-      errores.dni = "El DNI debe tener 8 números.";
+    //Alerta apellido
+    if (apellido.trim() === "") {errores.nombre = "Campo apellido no completado";}
+
+    //Alerta dni
+    if (dni.trim() === "") {errores.dni = "Campo DNI no completado";} 
+    if (dni.length !== 8) {errores.dni = "El DNI debe tener exactamente 8 números.";}
+
+    //Alerta fecha
+    if (fecha.trim() === "") {errores.fecha = "Campo fecha no completado";}
+    else {
+      const fechausuario = new Date(fecha);
+      const fechamax = new Date(FechaMax());
+      if (fechausuario > fechamax) {
+        errores.fecha = `La fecha no es válida, fecha máxima: ${fechamax.toLocaleString()}`;
+      }
     }
-  }
 
-  //Alerta fecha
-  if (fecha.trim() === "") {
-    errores.fecha = "Campo fecha no completado";
-  } else {
-    const fechausuario = new Date(fecha);
-    const fechamax = new Date(FechaMax());
-    if (fechausuario > fechamax) {
-      errores.fecha = `La fecha no es válida, fecha máxima: ${fechamax.toLocaleString()}`;
+    if (motivo.trim() === "") {errores.motivo = "Campo motivo no completado";}
+
+    // Guardado de errores para mostrarlo despues
+    setErrores(errores);
+
+    if (Object.keys(errores).length === 0) {
+      try {
+      await addDoc(collection(db, 'turnos'), { nombreCompleto: nombre + " " + apellido, dni, fecha, motivo, creadoEn: new Date(), }); //Campos que se van a enviar a la bd.
+      setNombre(''); 
+      setApellido('');
+      setDNI('');
+      setFecha(FechaActual());
+      setMotivo('');
+      navigate("/Recibido", { state: { nombre } });
+      } catch (error) {
+      console.error(error);
+      alert('Error al registrar turno.');
+      }
     }
-  }
-
-  if (motivo.trim() === "") {
-    errores.motivo = "Campo motivo no completado";
-  }
-
-  // Guardado de errores para mostrarlo despues
-  setErrores(errores);
-
-  if (Object.keys(errores).length === 0) {
-    alert("Formulario enviado");
-    navigate("/Recibido", { state: { nombre } });
-  }
-};
+  };
 
 //Estructura del formulario 
 return(
     <div className="formulario">
         <form onSubmit={handleSubmit} className="formu">
-            
             <div className="formtxt">Formulario de turno</div>
-
                 <div>
                 <label>Nombre:</label>
                 <input type="text"
                 value={nombre}
-                onChange={(e)=>setNombre(e.target.value.replace(/[^A-Za-z]/g, ""))}/>
+                onChange={(e)=>setNombre(e.target.value.replace(/[^A-Za-zÁÉÍÓÚáéíóúÑñ\s]/g, ""))}/>
                  {errores.nombre && <p className="mensaje-error">{errores.nombre}</p>}
             </div>
+              <div>
+                <label>Apellido:</label>
+                <input type="text"
+                value={apellido}
+                onChange={(e)=>setApellido(e.target.value.replace(/[^A-Za-zÁÉÍÓÚáéíóúÑñ\s]/g, ""))}/>
+                 {errores.nombre && <p className="mensaje-error">{errores.apellido}</p>}
+            </div> 
             <div>
                 <label>DNI:</label>
                 <input type="text" maxLength={8}
@@ -113,13 +124,12 @@ return(
                 <label>Motivo:</label>
                 <select value={motivo} onChange={(e)=>setMotivo(e.target.value)}>
                     <option value="">----------Elegir Motivo----------</option>
-                    <option value="opcion1">Hablar con un preceptor.</option>
-                    <option value="opcion2">Hablar con un regente.</option>
-                    <option value="opcion3">Hablar con un directivo.</option>
+                    <option value="Hablar con un preceptor.">Hablar con un preceptor.</option>
+                    <option value="Hablar con un regente.">Hablar con un regente.</option>
+                    <option value="Hablar con un directivo.">Hablar con un directivo.</option>
                 </select>
                  {errores.motivo && <p className="mensaje-error">{errores.motivo}</p>}
             </div>
-
             <button className="subt" type="submit">Enviar</button>
         </form>
     </div>
