f9dc2671d8e6cc72f10583526c19a532a07f710b
Baggio10 <francogarayanton1@gmail.com>
2025-10-12 04:49:41 +0200

    Modificación de la estructura ABM para darle estilo.

--- src/pages/ABM.jsx
+++ src/pages/ABM.jsx
@@ -5,35 +5,31 @@ import AltaAdmin from "../components/AltaAdmin";
 import ListaAdmins from "../components/ListaAdmins";
 
 const ABM = () => {
-    const [admin, setAdmin] = useState(false);
-    const [prece, setPrece] = useState(false);
+  const [admin, setAdmin] = useState(false);
+  const [prece, setPrece] = useState(false);
 
-    return (
-        <div className="cajaabm">
-            {/* Div con toda la page de ABM. */}
-            <div>
-                <div>
-                    {/* Div que contiene el menu de las colecciones. */}
-                    <div className="botonesabm">
-                        <div><button className="botad" onClick={() => setAdmin(true)}>Admins</button></div>
-                        <div><button className="botpre" onClick={() => setPrece(true)}>Preceptores</button></div>
-                    </div>
-                <div className="list">
-                    {prece && (
-                    <div >
-                        <div><AltaPreceptor /></div>
-                        <div><ListaPreceptores /></div>
-                    </div>
-                    )}
-                    {admin && (
-                    <div >
-                        <div><AltaAdmin /></div>
-                        <div><ListaAdmins /></div>
-                    </div>
-                    )}
-                </div>
-            </div>
-        </div>
+  return (
+    <div className="container">
+      <div className="sidebar">
+        <button onClick={() => { setAdmin(true); setPrece(false); }}>Admins</button>
+        <button onClick={() => { setPrece(true); setAdmin(false); }}>Preceptores</button>
+      </div>
+
+      <div className="content">
+        {prece && (
+          <div className="section">
+            <AltaPreceptor />
+            <ListaPreceptores />
+          </div>
+        )}
+
+        {admin && (
+          <div className="section">
+            <AltaAdmin />
+            <ListaAdmins />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
