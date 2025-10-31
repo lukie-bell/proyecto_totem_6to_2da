47cc9bf96f723ad2909f28dfdce37bb997729582
AgustinEliel <agustin.gonzalez@init-lab.com.ar>
2025-10-31 01:06:17 -0300

    se arreglo el app.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [user, setUser] = useState(null);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/proyecto_totem_6to_2da">
       <AppRouter user={user} setUser={setUser} />
     </BrowserRouter>
   );
